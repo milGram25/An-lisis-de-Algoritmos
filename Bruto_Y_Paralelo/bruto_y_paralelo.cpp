7dec5b8a5814c03dc1ac5be60df2fa40a120a337
Milton Garcia <milton.garcia8181@alumnos.udg.mx>
2026-02-14 11:32:01 -0600

    [ParaleloBruto] (refactor) -> Unir los procesos de los distintos algoritmos

--- Bruto_Y_Paralelo/bruto_y_paralelo.cpp
+++ Bruto_Y_Paralelo/bruto_y_paralelo.cpp
@@ -9,7 +9,26 @@
 
 using namespace std;
 
-atomic<bool> encontrado(false);
+atomic<int> encontrado(-1);
+
+int entrada(){
+    char input;
+    cin >> input;
+    if(isdigit(input)) {
+        return input - '0';
+    }
+    return -1;
+}
+
+auto iniciarTemporizador(){
+    return chrono::high_resolution_clock::now();
+}
+
+void tiempoEjecución(chrono::high_resolution_clock::time_point comienzo){
+    auto terminar = chrono::high_resolution_clock::now();
+    auto duracion = chrono::duration_cast<chrono::microseconds>(terminar - comienzo);
+    cout << "Tiempo de ejecución: " << duracion.count() << " microsegundos" << endl;
+}
 
 void buscar(int pin, int inicio, int final, promise<int>& resultado){
     for (int i = inicio; i < final; i++){
@@ -23,31 +42,67 @@ void buscar(int pin, int inicio, int final, promise<int>& resultado){
     }
 }
 
-int main(){
-    int hilos = 1;
-    int rango = 10000/hilos;
-    vector<future<int>> futuros;
+void paralelo(int pin) {
+    int hilos = thread::hardware_concurrency(); // Use all available cores
+    int rango = 10000 / hilos;
     vector<promise<int>> promesas(hilos);
+    vector<future<int>> futuros;
+    vector<thread> trabajadores;
 
-    srand(time(nullptr));
+    auto comienzo = iniciarTemporizador();
 
-    int pin = rand() % 10000;
-    auto comienzo = chrono::high_resolution_clock::now();
-
-    for(int i = 0; i < hilos; i++){
+    for (int i = 0; i < hilos; i++) {
         int inicio = i * rango;
         int final = (i == hilos - 1) ? 10000 : (i + 1) * rango;
-
         futuros.push_back(promesas[i].get_future());
-        thread t(buscar, pin, inicio, final, ref(promesas[i]));
-        t.detach();
+        trabajadores.emplace_back(buscar, pin, inicio, final, ref(promesas[i]));
     }
 
-    while(!encontrado.load()){}
+    for (auto& t : trabajadores) {
+        if (t.joinable()) t.join();
+    }
 
-    auto terminar = chrono::high_resolution_clock::now();
-    auto duracion = chrono::duration_cast<chrono::microseconds>(terminar - comienzo);
+    if (encontrado.load() != -1) {
+        cout << "Pin encontrado (Paralelo): " << encontrado.load() << endl;
+    }
 
-    cout << "Valor encontrado: " << futuros[0].get() << endl;
-    cout << "Tiempo de ejecución: " << duracion.count() << " microsegundos" << endl;
+    cout << "Busqueda paralela terminada." << endl;
+    tiempoEjecución(comienzo);
+}
+
+void bruto(int pin){ 
+    int intentos = 0;
+    int adivinanza = 0;
+
+    auto comienzo = iniciarTemporizador();   
+    
+    for (int i = 0; i <= 9999; i++) {
+        intentos++;     
+        adivinanza = i;
+        
+        if (adivinanza == pin) {
+            cout << "Pin encontrado: " << adivinanza << endl;
+            break;
+        }
+    }
+
+    tiempoEjecución(comienzo);
+}
+
+void menu(){
+    int pin = rand() % 10000;
+
+    cout << "Algoritmo paralelo" << endl;
+    paralelo(pin);
+
+    cout << "Algoritmo de fuerza bruta" << endl;
+    bruto(pin);
+
+    
+}
+
+int main(){
+    srand(time(0));
+
+    menu();
 }
